86f549e08b2f4c2a152380f34ec73d89d5dd5d79
Tabish Rizvi <tabishr@gmail.com>
2014-12-23 18:55:15 -0800

    Adding scene object: plane

---
+++ PlaneObject.hh
@@ -0,0 +1,18 @@
+#ifndef _PLANEOBJECT_HH_
+#define _PLANEOBJECT_HH_
+
+#include "SceneObject.hh"
+#include "Vector3F.hh"
+
+class PlaneObject: public SceneObject{
+
+  Vector3F surfaceNormal;
+  float distanceFromOrigin;
+
+  PlaneObject(const Vector3F &surfaceNormal, float distanceFromOrigin);
+  Vector3F & get_surfaceNormal(void) const;
+  float get_distanceFromOrigin(void) const;
+
+};
+
+#endif /* _PLANEOBJECT_HH_ */
