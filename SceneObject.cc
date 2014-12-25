dfce5ec6d5d25f1c689a1619a0404c5ba1b7d416
Tabish Rizvi <tabishr@gmail.com>
2014-12-25 11:31:27 -0800

    Fixing reference returning bugs

--- SceneObject.cc
+++ SceneObject.cc
@@ -1,18 +1,19 @@
 #include "SceneObject.hh"
+#include "RGBColor.hh"
 
 SceneObject :: SceneObject(void){
   surfaceColor = RGBColor(0.5, 0.5, 0.5);
 }
 
-SceneObject :: RGBColor get_surfaceColor(void) const{
+RGBColor SceneObject :: get_surfaceColor(void) const{
   return surfaceColor;
 }
 
-SceneObject :: void set_surfaceColor(const RGBColor &color){
+void SceneObject :: set_surfaceColor(const RGBColor &color){
   surfaceColor = color;
 }
 
-SceneObject :: RGBColor surfaceColorAtPoint(const Vector3F &point) const{
+RGBColor SceneObject :: surfaceColorAtPoint(const Vector3F &point) const{
   /* Simple implementation for now; will add textures later */
   return surfaceColor;
 }
