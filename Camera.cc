01be97d394677ae4c42a43e91fdba85db9cc8271
Tabish Rizvi <tabishr@gmail.com>
2015-02-14 13:09:03 -0800

    Finish implementation of Camera class.

--- Camera.cc
+++ Camera.cc
@@ -1,5 +1,6 @@
 #include "Camera.hh"
 #include "Vector3F.hh"
+#include "Ray.hh"
 
 #include <cmath>
 
@@ -12,5 +13,15 @@ Camera :: Camera(Vector3F loc, Vector3F lookAt, Vector3F up, float fieldOfView){
   this->cameraRight = this->dir ^ up;
   this->cameraUp = this->cameraRight ^ this->dir;
 
-  this->distance = 0.5 / tanf(this->fieldOfView / 2);
+  this->distance = 0.5 / tanf(this->fieldOfView / 2.0);
+}
+
+Ray Camera :: getRayForPixel(int x, int y, int imgSize) const{
+  
+  Vector3F pixelDir = this->distance * this->dir + 
+    (0.5 - (float)y / (float)(imgSize - 1)) * this->cameraUp +
+    ((float) x / (float)(imgSize - 1) - 0.5) * this->cameraRight;
+
+  Ray pixelRay(this->loc, pixelDir);
+  return pixelRay;
 }
--- Camera.hh
+++ Camera.hh
@@ -2,20 +2,23 @@
 #define _CAMERA_HH_
 
 #include "Vector3F.hh"
+#include "Ray.hh"
 
 #define SIXTY_DEG_TO_RAD 1.04719755
 
 class Camera{
 
   Vector3F loc;
-  Vector3F dir;
+  Vector3F dir; /* Normalized */
   float fieldOfView; /* In radians */
-  Vector3F cameraUp;
-  Vector3F cameraRight;
+  Vector3F cameraUp; /* Normalized */
+  Vector3F cameraRight; /* Normalized */
   float distance;
 
 public:
   Camera(Vector3F loc, Vector3F lookAt, Vector3F up, float fieldOfView = SIXTY_DEG_TO_RAD);
+
+  Ray getRayForPixel(int x, int y, int imgSize) const;
 };
 
 
